0c10e3443920e334ff25ab54d02a1ca1c8915ca4
Alper Sünter <41203420+alpersunter@users.noreply.github.com>
2019-05-27 13:52:39 +0300

    Create text2wav.py

---
+++ 3- Parlaklık - Ses/text2wav.py
@@ -0,0 +1,35 @@
+import numpy as np
+import wave
+import struct
+import math
+
+
+f = open("demofile.txt", "r")
+sesVektörü = f.read().split()
+sesVektörü = [ int(x) for x in sesVektörü]
+sesVektörü = np.array(sesVektörü)
+
+# SESE DÖNÜŞTÜRÜLÜYOR
+print("Wav olarak kaydediliyor...")
+sampleRate = 21649.0  # hertz
+ses_adı = f.name + "-.wav"
+
+wavef = wave.open(ses_adı, 'w')
+wavef.setnchannels(1)  # mono
+wavef.setsampwidth(2)
+wavef.setframerate(sampleRate)
+
+
+sesVektörü = sesVektörü - np.mean(sesVektörü)
+sesVektörü = sesVektörü / np.absolute(sesVektörü).max()
+
+
+
+for i in range(sesVektörü.size):
+    value = int(sesVektörü
+                [i]*32767.0)
+    data = struct.pack('<h', value)
+    wavef.writeframesraw(data)
+
+wavef.close()
+print(ses_adı + " kaydedildi.")
